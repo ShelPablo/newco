eec749e7378b0f7197f4f616bd07be4682fa89bf
Shel <Shel-ne@mail.ru>
2018-09-10 10:45:24 +0300

    Task done

--- src/main/java/com/andreitop/newco/aspect/LoggingAspect.java
+++ src/main/java/com/andreitop/newco/aspect/LoggingAspect.java
@@ -4,6 +4,9 @@ package com.andreitop.newco.aspect;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -22,4 +25,27 @@ public class LoggingAspect {
         logger.info(" ---> Method " + className + "." + methodName + " is about to be called");
     }
 
+    @After("com.andreitop.newco.aspect.PointcutContainer.repositorySave()")
+    public void afterRepoSave(JoinPoint joinPoint) {
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        logger.info(" ---> Method " + className + "." + methodName + " is done");
+    }
+
+    @Around("com.andreitop.newco.aspect.PointcutContainer.controllerFind()")
+    public Object aroundControllerFind(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        String methodName = joinPoint.getSignature().getName();
+        logger.info(" ---> Intercepting call of: " + methodName);
+        long t1 = System.currentTimeMillis();
+        try {
+            //put a pause here so we can register an execution time
+            Thread.sleep(1000L);
+            return joinPoint.proceed();
+        } finally {
+            long t2 = System.currentTimeMillis();
+            logger.info(" ---> Execution of " + methodName + " took: "
+                    + (t2 - t1) / 1000 + " ms.");
+        }
+    }
 }
--- src/main/java/com/andreitop/newco/aspect/PointcutContainer.java
+++ src/main/java/com/andreitop/newco/aspect/PointcutContainer.java
@@ -5,6 +5,15 @@ import org.aspectj.lang.annotation.Pointcut;
 public class PointcutContainer {
 
     @Pointcut("execution( * com.andreitop.newco.repository.*Repo*.find*())")
-    public void repositoryFind() {
-    }
+    public void repositoryFind() {    }
+
+    @Pointcut("execution( * com.andreitop.newco.repository.*Repo*.save(..))")
+    public void repositorySave() {    }
+
+    @Pointcut("execution( * com.andreitop.newco.controller.*Control*.find*())")
+    public void controllerFind() {    }
+
+    @Pointcut("execution( * com.andreitop.newco.controller.*Repo*.save(..))")
+    public void controllerSave() {    }
+
 }
