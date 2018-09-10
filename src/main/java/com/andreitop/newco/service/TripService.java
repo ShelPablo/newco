785984cabab868ec19bfa1e6a08d5903bd45f4cb
Shel <Shel-ne@mail.ru>
2018-09-10 13:16:44 +0300

    Methods save, delete, update marked as @Transactional

--- src/main/java/com/andreitop/newco/service/TripService.java
+++ src/main/java/com/andreitop/newco/service/TripService.java
@@ -4,6 +4,8 @@ import com.andreitop.newco.dto.TripDto;
 import com.andreitop.newco.repository.TripRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @Service
@@ -23,15 +25,15 @@ public class TripService {
     public TripDto findById(Long id) {
         return tripRepository.findById(id).get();
     }
-
+    @Transactional
     public void save(TripDto trip) {
         tripRepository.save(trip);
     }
-
+    @Transactional
     public void delete(Long id) {
         tripRepository.deleteById(id);
     }
-
+    @Transactional
     public void update(TripDto newTrip) {
         tripRepository.save(newTrip);
     }
